8ad5cd4a483dde2e97589271621bc27e1eef93ef
Sergey Kacheev <skacheev@yandex-team.ru>
2018-11-14 14:18:31 +0700

    fix fist quiz

--- week1/aplusb.rs
+++ week1/aplusb.rs
@@ -1,12 +1,11 @@
 use std::io::{self, Read};
 
-pub fn main() -> io::Result<()> {
+pub fn main() {
     let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf)?;
+    io::stdin().read_to_string(&mut buf).unwrap();
     let mut iter = buf.trim().split_whitespace();
     let a: i32 = iter.next().unwrap().parse().unwrap();
     let b: i32 = iter.next().unwrap().parse().unwrap();
 
     println!("{}", a + b);
-    Ok(())
 }
