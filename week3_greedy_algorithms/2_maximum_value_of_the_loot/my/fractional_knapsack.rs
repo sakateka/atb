694deb9fb5ee9c64e7c2ddb3e002c2868ef8a5eb
Sergey Kacheev <skacheev@yandex-team.ru>
2018-11-24 18:23:23 +0700

    fractional_knapsack: fix precision

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/my/fractional_knapsack.rs
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/my/fractional_knapsack.rs
@@ -1,11 +1,11 @@
 use std::io::{self, Read};
 use std::cmp::Ordering::Equal;
 
-fn min(a: f32, b: f32) -> f32 {
+fn min(a: f64, b: f64) -> f64 {
     if a < b { a } else { b }
 }
 
-fn get_optimal_value(mut capacity: f32, mut items: Vec<Item>) -> f32 {
+fn get_optimal_value(mut capacity: f64, mut items: Vec<Item>) -> f64 {
     let mut v = 0.;
     let mut index: Vec<usize> = (0..items.len()).collect::<Vec<usize>>();
     index.sort_by(|a, b|{
@@ -15,7 +15,7 @@ fn get_optimal_value(mut capacity: f32, mut items: Vec<Item>) -> f32 {
         cost_b.partial_cmp(&cost_a).unwrap_or(Equal)
     });
     for i in index {
-        if capacity == 0.0 {
+        if capacity == 0.00000 {
             break;
         }
         let add_w = min(items[i].weight, capacity);
@@ -27,15 +27,15 @@ fn get_optimal_value(mut capacity: f32, mut items: Vec<Item>) -> f32 {
 }
 
 struct Item {
-    weight: f32,
-    value: f32,
+    weight: f64,
+    value: f64,
 }
 
 
 pub fn main() {
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf).unwrap();
-    let numbers: Vec<f32> = buf.trim().split_whitespace()
+    let numbers: Vec<f64> = buf.trim().split_whitespace()
         .map(|x| x.parse().unwrap()).collect();
 
     let n = numbers[0] as u32;
@@ -48,5 +48,5 @@ pub fn main() {
         }
     }).collect();
 
-    println!("{:0.10}", get_optimal_value(capacity, items));
+    println!("{:0.3}", get_optimal_value(capacity, items));
 }
