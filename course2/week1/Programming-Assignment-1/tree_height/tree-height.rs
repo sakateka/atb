9d9c3613cdc1a71ca570264c550f904143122fc6
Sergey Kacheev <skacheev@yandex-team.ru>
2019-06-08 00:33:39 +0700

    complete tree-height.rs

--- course2/week1/Programming-Assignment-1/tree_height/tree-height.rs
+++ course2/week1/Programming-Assignment-1/tree_height/tree-height.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp;
 
 #[derive(Debug)]
 struct Node {
@@ -17,12 +18,22 @@ impl Node {
     }
 }
 
-pub fn main() {
+fn tree_height(tree: &Vec<Node>, from: usize) -> usize {
+    let node = &tree[from];
+    let mut max_size = 0;
+    for c in node.children.iter() {
+        max_size = cmp::max(max_size, tree_height(tree, tree[*c].idx));
+    }
+    max_size + 1
+}
+
+fn run() {
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).unwrap();
     let size = buf[..buf.len()-1].parse::<usize>().unwrap();
     buf.clear();
 
+    let mut root: usize = 0;
     let mut tree = (0..size).map(|idx|Node::new(idx)).collect::<Vec<Node>>();
 
     // populate
@@ -33,6 +44,25 @@ pub fn main() {
         tree[idx].parent = parent;
         if parent > -1 {
             tree[parent as usize].children.push(idx);
+        } else {
+            root = idx;
         }
     }
+
+    let height = tree_height(&tree, root);
+    println!("{}", height);
+}
+
+use std::thread;
+const STACK_SIZE: usize = 16 * 1024 * 1024;
+
+fn main() {
+    // Spawn thread with explicit stack size
+    let child = thread::Builder::new()
+        .stack_size(STACK_SIZE)
+        .spawn(run)
+        .unwrap();
+
+    // Wait for thread to join
+    child.join().unwrap();
 }
