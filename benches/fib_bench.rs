5a9e072db0cd363f6aed40bc245ad1717edabe3c
Sergey Kacheev <s.kacheev@gmail.com>
2022-08-26 19:50:33 +0700

    add criterion

--- benches/fib_bench.rs
+++ benches/fib_bench.rs
@@ -1,17 +1,13 @@
-extern crate test;
+use algorithmic_toolbox::math::{fib, fib_naive};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-#[cfg(test)]
-mod tests {
-
-    use crate::math::{fib, fib_naive};
-    use test::Bencher;
-
-    #[bench]
-    fn bench_fib(b: &mut Bencher) {
-        b.iter(|| fib(10));
-    }
-    #[bench]
-    fn bench_fib_naive(b: &mut Bencher) {
-        b.iter(|| fib_naive(10));
-    }
+fn benchmark1(c: &mut Criterion) {
+    c.bench_function("fib 10", |b| b.iter(|| fib(black_box(10))));
 }
+
+fn benchmark2(c: &mut Criterion) {
+    c.bench_function("fib_naive 10", |b| b.iter(|| fib_naive(black_box(10))));
+}
+
+criterion_group!(benches, benchmark1, benchmark2);
+criterion_main!(benches);
